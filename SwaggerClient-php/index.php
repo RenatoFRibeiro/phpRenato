f8c48fc944aa548cd28a9ecd18361849824f324e
Renato Ribeiro <rf-ribeiro@criticalsoftware.com>
2019-10-11 14:22:56 +0100

    update normal

---
+++ SwaggerClient-php/index.php
@@ -0,0 +1,196 @@
+<?php
+
+require_once(__DIR__ . '/vendor/autoload.php');
+/*
+$apiInstance = new Swagger\Client\Api\EmployeesApiControllerApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client()
+);
+$body_limit = 56; // int | The amount of employees returned
+$page_limit = 56; // int | The pages to  return employees info
+
+try {
+    $result = $apiInstance->employeesGet($body_limit, $page_limit);
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling EmployeesApiControllerApi->employeesGet: ', $e->getMessage(), PHP_EOL;
+}
+*/
+// define variables and set to empty values
+$nameErr = $idErr = $titleErr = "";
+$name = $id = $title = "";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name =$_POST["name"];
+  }
+  if (empty($_POST["title"])) {
+    $titleErr = "Title is required";
+  } else {
+    $title = $_POST["title"];
+  }
+  if (empty($_POST["id"])) {
+    $idErr = "ID is required";
+  } else {
+    $id = $_POST["id"];
+  }
+
+
+  $apiInstance = new Swagger\Client\Api\EmployeesApiControllerApi(new GuzzleHttp\Client());
+  $body = new \Swagger\Client\Model\Employee(); // \Swagger\Client\Model\Employee | body
+
+try {
+    $apiInstance->employeesPost($body);
+} catch (Exception $e) {
+    echo 'Exception when calling EmployeesApiControllerApi->employeesPost: ', $e->getMessage(), PHP_EOL;
+}
+
+
+/*
+$e = array('employee_name' => $name, 'employee_title' => $title, 'id' => $id);
+$emp = new \Swagger\Client\Model\Employee();
+$api = new Swagger\Client\Api\EmployeesApiControllerApi(new GuzzleHttp\Client());
+$api->employeesPost($emp);
+*/
+}
+
+/*function test_input($data) {
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+return $data;
+}*/
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Free Web tutorials">
+  <meta name="keywords" content="HTML,CSS,XML,JavaScript">
+  <meta name="author" content="Renato Ribeiro">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Employees API</title>
+</head>
+
+<body>
+  <h1>Employees API</h1>
+  <div id="buttondiv">
+    <button class="button topbtn">Add Employee</button>
+    <button class="button topbtn">Delete Employee</button>
+    <button class="button topbtn">Get Employee</button>
+    <button class="button topbtn">Edit Employee</button>
+    <button class="button topbtn">List Employees</button>
+  </div>
+  <div id="boarddiv">
+    <p style="text-align: center;">Board:</p>
+    <div id="addoption">
+      <p>Add Option</p>
+      <form method="post" action="index.php">
+        Employee ID:
+        <input type="number" name="id" placeholder="ID">
+        <br>
+        Employee Name:
+        <input type="text" name="name" placeholder="NAME">
+        <br>
+        Employee Title:
+        <input type="text" name="title" placeholder="TITLE">
+        <br><br>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+    <div id="deleteoption">
+      <p>Delete Option</p>
+      <form action="">
+        Employee ID:
+        <input type="text" name="" value="">
+        <br><br>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+    <div id="getoption">
+      <p>Get Option</p>
+      <form action="">
+        Employee ID:
+        <input type="number" name="id" value="ID">
+        <span class="error"><?php echo $idErr; ?></span>
+        <br><br>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+    <div id="listoption">
+      <p>List Option</p>
+      <p>List:</p>
+    </div>
+    <div id="editoption">
+      <p>Edit Option</p>
+      <form method="post" action="index.php">
+        Employee ID:
+        <input type="number" name="id" value="ID">
+        <br>
+        Employee Name:
+        <input type="text" name="employee name" value=" NAME">
+        <br>
+        Employee Title:
+        <input type="text" name="employee title" value="TITLE">
+        <br><br>
+        <input type="submit" value="Submit">
+      </form>
+    </div>
+  </div>
+  <!------------------------------------------------------------------------------------>
+
+  <div class="col">
+    <div class="insideCol">
+      <h5>Employees List</h5>
+      <p>
+        <?php
+            echo $emp;
+            ?>
+      </p>
+    </div>
+  </div>
+
+
+  <div class="col getEmp">
+    <div class="insideCol">
+      <h5>Get Employee (by ID)</h5>
+      <div class="getInside" style="padding: 5px; transform: scale(0.9)">
+        <form method="post" action="index.php">
+          <label> ID: </label>
+          <input type="number" name="id">
+          <span class="error"><?php echo $idErr; ?></span>
+          <br><br>
+          <label> Nome: </label> <input type="text" name="name">
+          <span class="error"><?php echo $nameErr; ?></span>
+          <br><br>
+          <label> Title: </label>
+          <input type="text" name="title">
+          <span class="error"><?php echo $titleErr; ?></span>
+          <br><br>
+          <input type="submit" name="submit" value="Submit">
+        </form>
+      </div>
+    </div>
+  </div>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+    integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous">
+  </script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+    integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
+  </script>
+
+</body>
+
+</html>
