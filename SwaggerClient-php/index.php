4348bdfe1e2ce2342ac6ba671cd9a8878f724d4a
Renato Ribeiro <rf-ribeiro@criticalsoftware.com>
2019-10-14 15:27:05 +0100

    alteraçoes de segunda feira, metodos implementados, menos o GET eo List ainda não está como deve ser!

--- SwaggerClient-php/index.php
+++ SwaggerClient-php/index.php
@@ -1,83 +1,100 @@
 <?php
-
 require_once(__DIR__ . '/vendor/autoload.php');
-/*
-$apiInstance = new Swagger\Client\Api\EmployeesApiControllerApi(
-    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
-    // This is optional, `GuzzleHttp\Client` will be used as default.
-    new GuzzleHttp\Client()
-);
-$body_limit = 56; // int | The amount of employees returned
-$page_limit = 56; // int | The pages to  return employees info
-
-try {
-    $result = $apiInstance->employeesGet($body_limit, $page_limit);
-    print_r($result);
-} catch (Exception $e) {
-    echo 'Exception when calling EmployeesApiControllerApi->employeesGet: ', $e->getMessage(), PHP_EOL;
-}
-*/
+ 
+include("lib/Api/EmployeesApiControllerApi.php");
+include("lib/Model/Employee.php");
 // define variables and set to empty values
 $nameErr = $idErr = $titleErr = "";
 $name = $id = $title = "";
 $apiInstance = new Swagger\Client\Api\EmployeesApiControllerApi(new GuzzleHttp\Client());
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["name"])) {
-    $nameErr = "Name is required";
-  } else {
-    $name =$_POST["name"];
-  }
-  if (empty($_POST["title"])) {
-    $titleErr = "Title is required";
-  } else {
-    $title = $_POST["title"];
-  }
-  if (empty($_POST["id"])) {
-    $idErr = "ID is required";
-  } else {
-    $id = $_POST["id"];
-  }
-
-
-
-  $body = new \Swagger\Client\Model\Employee(); // \Swagger\Client\Model\Employee | body
-
-  $body->setEmployeeName($name);
-  $body->setEmployeeTitle($title);
-  $body->setId($id);
-
-
  
-try {
-  $emps = $apiInstance->employeesGet();
-  $apiInstance->employeesPost($body);
-
-  $emp = $apiInstance->employeesIdGet($id);
-  echo "ID: " . $emp->getID();
-  echo "<br> Name: " . $emp->getEmployeeName();
-  echo "<br> Title: " . $emp->getEmployeeTitle();
-
-} catch (Exception $e) {
-    echo 'Exception when calling EmployeesApiControllerApi->employeesPost: ', $e->getMessage(), PHP_EOL;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$action = $_POST['action'];
+  if ($action == "add"){
+    // adiciona
+    if (empty($_POST["name"])) {
+      $nameErr = "Name is required";
+    } else {
+      $name =$_POST["name"];
+    }
+    if (empty($_POST["title"])) {
+      $titleErr = "Title is required";
+    } else {
+      $title = $_POST["title"];
+    }
+    if (empty($_POST["id"])) {
+      $idErr = "ID is required";
+    } else {
+      $id = $_POST["id"];
+    }
+      $body = new \Swagger\Client\Model\Employee();
+      $body->setEmployeeName($name);
+      $body->setEmployeeTitle($title);
+      $body->setId($id);
+    try {
+      $apiInstance->employeesPost($body);
+      $emp = $apiInstance->employeesIdGet($id);
+      echo "ID: " . $emp->getID();
+    } catch (Exception $e) {
+      echo 'Exception when calling EmployeesApiControllerApi->employeesPost: ', $e->getMessage(), PHP_EOL;
+    }
+  } else if ($action == "del")
+  {
+    if (empty($_POST["id"])) {
+      $idErr = "ID is required";
+    } else {
+      $id = $_POST["id"];
+    }
+      echo "ID to delete: " . $id;
+    try {
+      $result = $apiInstance->employeesIdDelete($id);
+      print_r($result);
+    } catch (Exception $e) {
+      echo 'Exception when calling EmployeesApiControllerApi->employeesIdDelete: ', $e->getMessage(), PHP_EOL;
+    }
+  }else if ($action == "edit")
+  {
+    if (empty($_POST["id"])) {
+      $idErr = "ID is required";
+    } else {
+      $id = $_POST["id"];
+    }
+    try {
+    $result = $apiInstance->employeesIdDelete($id);
+      print_r($result);
+    } catch (Exception $e) {
+      echo 'Exception when calling EmployeesApiControllerApi->employeesIdDelete: ', $e->getMessage(), PHP_EOL;
+    }
+    // adiciona
+    if (empty($_POST["name"])) {
+      $nameErr = "Name is required";
+    } else {
+      $name =$_POST["name"];
+    }
+    if (empty($_POST["title"])) {
+      $titleErr = "Title is required";
+    } else {
+      $title = $_POST["title"];
+    }
+    if (empty($_POST["id"])) {
+      $idErr = "ID is required";
+    } else {
+      $id = $_POST["id"];
+    }
+    $body1 = new \Swagger\Client\Model\Employee();
+    $body1->setEmployeeName($name);
+    $body1->setEmployeeTitle($title);
+    $body1->setId($id);
+    try {
+      $apiInstance->employeesPost($body1);
+      $emp1 = $apiInstance->employeesIdGet($id);
+      echo "ID: " . $emp1->getID();
+    } catch (Exception $e) {
+      echo 'Exception when calling EmployeesApiControllerApi->employeesPost: ', $e->getMessage(), PHP_EOL;
+  }
 }
-
-
-/*
-$e = array('employee_name' => $name, 'employee_title' => $title, 'id' => $id);
-$emp = new \Swagger\Client\Model\Employee();
-$api = new Swagger\Client\Api\EmployeesApiControllerApi(new GuzzleHttp\Client());
-$api->employeesPost($emp);
-*/
+ 
 }
-
-/*function test_input($data) {
-$data = trim($data);
-$data = stripslashes($data);
-$data = htmlspecialchars($data);
-return $data;
-}*/
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -94,82 +111,112 @@ return $data;
 
 <body>
   <h1>Employees API</h1>
-  <div id="buttondiv">
-    <button class="button topbtn">Add Employee</button>
-    <button class="button topbtn">Delete Employee</button>
-    <button class="button topbtn">Get Employee</button>
-    <button class="button topbtn">Edit Employee</button>
-    <button class="button topbtn">List Employees</button>
-  </div>
-  <div id="boarddiv">
-    <p style="text-align: center;">Board:</p>
-    <div id="addoption">
-      <p>Add Option</p>
-      <form method="post" action="index.php">
-        Employee ID:
-        <input type="number" name="id" placeholder="ID">
-        <br>
-        Employee Name:
-        <input type="text" name="name" placeholder="NAME">
-        <br>
-        Employee Title:
-        <input type="text" name="title" placeholder="TITLE">
-        <br><br>
-        <input type="submit" value="Submit">
-      </form>
+  <div style="width: 100%; overflow: hidden;">
+    <div id="left">
+      <div id="buttondiv">
+        <button class="button topbtn" onclick="addoptionFunc()">Add Employee</button>
+        <button class="button topbtn" onclick="deleteoptionFunc()">Delete Employee</button>
+        <button class="button topbtn" onclick="getoptionFunc()">Get Employee</button>
+        <button class="button topbtn" onclick="editoptionFunc()">Edit Employee</button>
+      </div>
+      <div id="boarddiv">
+        <div id="addoption" class="hidenDiv">
+          <p>Add Option</p>
+          <form method="post" action="index.php">
+            Employee ID:
+            <input type="number" name="id" placeholder="ID">
+            <br>
+            Employee Name:
+            <input type="text" name="name" placeholder="NAME">
+            <br>
+            Employee Title:
+            <input type="text" name="title" placeholder="TITLE">
+            <br><br>
+            <input type="hidden" name="action" value="add"  />
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+        <br><br><br>
+        <div id="deleteoption" class="hidenDiv">
+          <p>Delete Option</p>
+          <form method="post" action="index.php">
+            <label> ID: </label>
+            <input type="number" name="id">
+            <span class="error"><?php echo $idErr; ?></span>
+            <br><br>
+            <input type="hidden" name="action" value="del"  />
+            <input type="submit" name="submit" value="Submit">
+          </form>
+        </div>
+        <br><br><br>
+        <div id="getoption" class="hidenDiv">
+          <p>Get Option</p>
+          <form action="post">
+            Employee ID:
+            <input type="number" name="id" value="ID">
+            <span class="error"><?php echo $idErr; ?></span>
+            <br><br>
+            <?php
+            $action = $_POST['action'];
+              if($action == "get"){
+                if (empty($_POST["id"])) {
+                  $idErr = "ID is required";
+                } else {
+                  $id = $_POST["id"];
+                }
+                try {
+                  $result = $apiInstance->employeesIdGet($id);
+                  prnt_r($id);
+                  print_r($result);
+                } catch (Exception $e) {
+                  echo 'Exception when calling EmployeesApiControllerApi->employeesIdGet: ', $e->getMessage(), PHP_EOL;
+                }
+              }
+            ?>
+            <input type="hidden" name="action" value="get"  />
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+        <br><br><br>
+        <div id="editoption" class="hidenDiv">
+          <p>Edit Option</p>
+          <form method="post" action="index.php">
+            Employee ID:
+            <input type="number" name="id" placeholder="ID">
+            <br>
+            Employee Name:
+            <input type="text" name="name" placeholder=" NAME">
+            <br>
+            Employee Title:
+            <input type="text" name="title" placeholder="TITLE">
+            <br><br>
+            <input type="hidden" name="action" value="edit"  />
+            <input type="submit" value="Submit">
+          </form>
+        </div>
+      </div>
     </div>
-    <br><br><br>
-    <div id="deleteoption">
-      <p>Delete Option</p>
-      <form action="">
-        Employee ID:
-        <input type="text" name="" value="">
-        <br><br>
-        <input type="submit" value="Submit">
-      </form>
-    </div>
-    <br><br><br>
-    <div id="getoption">
-      <p>Get Option</p>
-      <form action="">
-        Employee ID:
-        <input type="number" name="id" value="ID">
-        <span class="error"><?php echo $idErr; ?></span>
-        <br><br>
-        <input type="submit" value="Submit">
-      </form>
-    </div>
-    <br><br><br>
-    <div id="listoption">
-      <p>List Option</p>
-      <?php
-      require_once(__DIR__ . '/vendor/autoload.php');
-      $body_limit = 56; // int | The amount of employees returned
-      $page_limit = 56; // int | The pages to  return employees info
-      
-      try {
-          $result = $apiInstance->employeesGet($body_limit, $page_limit);
-          print_r($result);
-      } catch (Exception $e) {
-          echo 'Exception when calling EmployeesApiControllerApi->employeesGet: ', $e->getMessage(), PHP_EOL;
-      }
-      ?>
-    </div>
-    <br><br><br>
-    <div id="editoption">
-      <p>Edit Option</p>
-      <form method="post" action="index.php">
-        Employee ID:
-        <input type="number" name="id" placeholder="ID">
-        <br>
-        Employee Name:
-        <input type="text" name="employee name" placeholder=" NAME">
-        <br>
-        Employee Title:
-        <input type="text" name="employee title" placeholder="TITLE">
-        <br><br>
-        <input type="submit" value="Submit">
-      </form>
+    <div id="right">
+      <div>
+          <h2>Emplyee List</h2>
+          <?php
+          $body_limit = 56; // int | The amount of employees returned
+          $page_limit = 2; // int | The pages to  return employees info
+
+              $result = $apiInstance->employeesGet($body_limit, $page_limit);
+
+              foreach ($result as list("id" => $idLis, "employee_name" => $nameLis)) {
+              while (list ($idLis, $nameLis, $titleLis) = $result) {
+                  echo "<ul>\n" .
+                      "<li>$idLis</li>\n" .
+                      "<li>$nameLis</li>\n" .
+                      "<li>$titleLis</li>\n" .
+                      "</ul>\n";
+                      break;
+                  }
+              }
+          ?>
+        </div>
     </div>
   </div>
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
@@ -183,6 +230,54 @@ return $data;
     integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous">
   </script>
 
+  <script>
+    var xpost = document.getElementById("addoption");
+    var xdelete = document.getElementById("deleteoption");
+    var xget = document.getElementById("getoption");
+    var xedit = document.getElementById("editoption");
+
+  function addoptionFunc() {
+    if (xpost.style.display === "block") {
+      xpost.style.display = "none";
+    } else {
+      xpost.style.display = "block";
+      xdelete.style.display = "none";
+      xget.style.display = "none";
+      xedit.style.display = "none";
+
+    }
+  }
+  function deleteoptionFunc() {
+    if (xdelete.style.display === "block") {
+      xdelete.style.display = "none";
+    } else {
+      xdelete.style.display = "block";
+      xpost.style.display = "none";
+      xget.style.display = "none";
+      xedit.style.display = "none";
+    }
+  }
+  function getoptionFunc() {
+    if (xget.style.display === "block") {
+      xget.style.display = "none";
+    } else {
+      xget.style.display = "block";
+      xpost.style.display = "none";
+      xdelete.style.display = "none";
+      xedit.style.display = "none";
+    }
+  }
+  function editoptionFunc() {
+    if (xedit.style.display === "block") {
+      xedit.style.display = "none";
+    } else {
+      xedit.style.display = "block";
+      xpost.style.display = "none";
+      xdelete.style.display = "none";
+      xget.style.display = "none";
+    }
+  }
+  </script>
 </body>
 
 </html>
